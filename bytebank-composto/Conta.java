9df0b59221384c759ae6b0580d746cddc4466a4e
Jose Antonio <leao-gomes@hotmail.com>
2022-05-08 21:22:42 -0300

    Aula getters e setters de referencia

--- bytebank-composto/Conta.java
+++ bytebank-composto/Conta.java
@@ -2,9 +2,10 @@
 
 
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
+    //no atributo privado o saldo nao pode ser lido e nem modificado em nenhum lugar, mais para baixo criamos um metodo para retornar o saldo em outro lugar.
+    private double saldo;
+    private int agencia;
+    private int numero;
 
     //aqui é feito uma associação entre o objeto conta e o objeto cliente
     Cliente titular;
@@ -39,4 +40,25 @@ public class Conta {
     }
     }
 
+    //como o saldo esta privado, precisamos criar um metodo para retornar o saldo para outro lugar, somente assim pode jogar estes dados para outo  lugar
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int novoNumero) {
+        this.numero = novoNumero;
+    }
+
+
+    public void setTitular (Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
 }
--- bytebank-composto/TestaBanco.java
+++ bytebank-composto/TestaBanco.java
@@ -1,9 +1,12 @@
 public class TestaBanco {
     public static void main(String[] args) {
         Cliente paulo = new Cliente();
-        paulo.nome = "Paulo Silveira";
-        paulo.cpf = "111.111.111-11";
-        paulo.profissao = "Desenvolvedor";
+        //paulo.nome = "Paulo Silveira";
+        paulo.setNome("Paulo Silveira");
+        
+
+        //paulo.cpf = "111.111.111-11";
+        //paulo.profissao = "Desenvolvedor";
 
         Conta contaDoPaulo = new Conta();
         //contaDoPaulo.deposita = (100);
@@ -11,7 +14,15 @@ public class TestaBanco {
         //associa o cliente paulo a conta contaDoPaulo
         contaDoPaulo.titular = paulo;
 
-        System.out.println(contaDoPaulo.titular.nome);
+        //System.out.println(contaDoPaulo.titular.nome);
+
+        //teste em pegar o saldo privado dentro do conta.java
+        Conta conta = new Conta();
+        conta.deposita(100);
+        System.out.println(conta.getSaldo());
+
+        //setando a conta do paulo
+        conta.setTitular(paulo);
 
     }
 }
